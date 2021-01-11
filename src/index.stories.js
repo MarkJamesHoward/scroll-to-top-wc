140b403c9a40deef305809ff38bb6956521e7f39
Mark Howard <markjameshoward@hotmail.co.uk>
2021-01-11 02:08:13 +0000

    first storyboard updated formatting

--- src/index.stories.js
+++ src/index.stories.js
@@ -11,10 +11,30 @@ export const story1 = () => html`
   <head>
     <style>
       :root {
-        --scroll-top-background-color: blue;
+        --scroll-top-background-color: #BADA55;
       }
+      .text {
+      height:200vh;
+      width: 500px;
+      background: #C55;
+      display:flex;
+      align-items:center;
+      flex-direction:column;
+      }
+
+      p {
+      margin: 1rem;
+      }
+
     </style>
   </head>
-  <div style="height:1500px;width: 500px;background: #BADA55;">test</div>
+  <div class="text">
+  <p>
+  Scroll down in order for the button to display
+  </p>
+  <p>
+  This one is set to the default scroll distance before displaying the scroll to top button
+  </p>
+  </div>
   <scroll-to-top-wc></scroll-to-top-wc>
 `;
